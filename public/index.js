cdda50a5cb8a35ef358aa1adec173cae6a0c6aba
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-07-19 14:02:20 +0800

    Add another canvas that displays prediction result

--- public/index.js
+++ public/index.js
@@ -1,11 +1,24 @@
-const canvas = document.getElementById('digit-canvas');
+import { submitInput } from './api/predict.js';
 
-const ctx = canvas.getContext('2d');
+const drawCanvas = document.getElementById('digit-canvas');
+const resultCanvas = document.getElementById('result-canvas');
+const submitBtn = document.querySelector('.submit');
+const clearBtn = document.querySelector('.clear');
+
+const drawCtx = drawCanvas.getContext('2d');
+const resultCtx = resultCanvas.getContext('2d');
+
 const drawBoard = document.getElementById('draw-board');
 const drawBoardStyles = getComputedStyle(drawBoard); // Get all the styles of drawBoard
 
-canvas.width = parseInt(drawBoardStyles.getPropertyValue('width'));
-canvas.height = parseInt(drawBoardStyles.getPropertyValue('height'));
+const resultBoard = document.getElementById('result-board');
+const resultBoardStyles = getComputedStyle(resultBoard);
+
+drawCanvas.width = parseInt(drawBoardStyles.getPropertyValue('width'));
+drawCanvas.height = parseInt(drawBoardStyles.getPropertyValue('height'));
+
+resultCanvas.width = parseInt(resultBoardStyles.getPropertyValue('width'));
+resultCanvas.height = parseInt(resultBoardStyles.getPropertyValue('height'));
 
 // Initialize mouse position
 const mouse = {
@@ -14,57 +27,61 @@ const mouse = {
 }
 
 // Calculate mouse position
-canvas.addEventListener('mousemove', function (e) {
+drawCanvas.addEventListener('mousemove', function (e) {
     // Take mouse position and subtract from the parent div's offset position
     mouse.x = e.pageX - this.offsetLeft;
     mouse.y = e.pageY - this.offsetTop;
 });
 
-ctx.lineWidth = 5;
-ctx.lineJoin = 'round'; // Create a rounded corner when two lines meet
-ctx.lineCap = 'round'; // Draw a line with rounded end caps
-ctx.strokeStyle = '#0000FF';
+drawCtx.lineWidth = 5;
+drawCtx.lineJoin = 'round'; // Create a rounded corner when two lines meet
+drawCtx.lineCap = 'round'; // Draw a line with rounded end caps
+drawCtx.strokeStyle = 'rgb(13, 152, 186)';
 
-canvas.addEventListener('mousedown', function () {
-    ctx.moveTo(mouse.x, mouse.y); // Move to click position
-    ctx.beginPath(); // Begin a path
-    canvas.addEventListener('mousemove', onPaint);
+resultCtx.font = "100px Georgia";
+
+drawCanvas.addEventListener('mousedown', function () {
+    drawCtx.moveTo(mouse.x, mouse.y); // Move to initial position
+    drawCtx.beginPath(); // Begin a path
+    drawCanvas.addEventListener('mousemove', onPaint);
 });
 
-canvas.addEventListener('mouseup', function () {
-    canvas.removeEventListener('mousemove', onPaint);
-    const img = new Image();
-    img.onload = async function () {
-        ctx.drawImage(img, 0, 0, 28, 28); // Draw image on canvas. Scale down image to 28 by 28
-        const imgData = ctx.getImageData(0, 0, 28, 28).data;
-        const input = [];
-        // Data is in the form of rgba
-        // Since the canvas is drawn with blue color, we need to extract the data in the third channel only (think 0 to 255 as grayscale)
-        for (let c = 0; c < imgData.length; c += 4) {
-            input.push(imgData[c + 2] / 255);
-        }
-        fetch('/predict', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                data: input
-            })
-        }).then(res => {
-            return res.json();
-        }).then(data => {
-            if (data && data.error) {
-                alert(data.error);
-            } else {
-                console.log(data.prediction);
-            }
-        });
+drawCanvas.addEventListener('mouseup', function () {
+  drawCanvas.removeEventListener('mousemove', onPaint);
+});
+
+submitBtn.addEventListener('click', function () {
+  const img = new Image();
+  // async function that runs after image is loaded
+  img.onload = async function () {
+    drawCtx.drawImage(img, 0, 0, 28, 28); // Draw image at position (0, 0) of canvas. Scale to 28 by 28
+    const imgPixels = drawCtx.getImageData(0, 0, 28, 28);
+    const input = [];
+    // imgData is in the form of rgba. Hence, each pixel has 4 values
+    for (let c = 0; c < imgPixels.data.length; c += 4) {
+      // Convert img to grayscale by calculating the average of rgb values
+      const avgPixel = (imgPixels.data[c] + imgPixels.data[c + 1] + imgPixels.data[c + 2]) / 3;
+      input.push(avgPixel / 255.0); // Normalize pixel
     }
-    img.src = canvas.toDataURL('image/png');
+    // Send prediction to trained model
+    submitInput(input).then(data => {
+      if (data && data.error) {
+        alert(data.error);
+        return;
+      } else {
+        resultCtx.fillText(`${data.prediction}`, 150, 200);
+      }
+    })
+  }
+  img.src = drawCanvas.toDataURL('image/png');
+});
+
+clearBtn.addEventListener('click', function () {
+  drawCtx.clearRect(0, 0, drawCanvas.width, drawCanvas.height);
+  resultCtx.clearRect(0, 0, resultCanvas.width, resultCanvas.height);
 });
 
 function onPaint() {
-    ctx.lineTo(mouse.x, mouse.y);
-    ctx.stroke();
+    drawCtx.lineTo(mouse.x, mouse.y);
+    drawCtx.stroke();
 }
